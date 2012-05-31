9584641253c35d9da6724d8101eb19834c9d0b9c
subhaze <jmrussell22@gmail.com>
2012-05-31 11:32:26 -0400

    Added simple event emitter

---
+++ emi.js
@@ -0,0 +1,48 @@
+/*
+---
+includes: emi by Valerio Proietti (https://github.com/kamicane/emi)
+...
+*/
+
+// emi: a stupidly tiny event emitter
+// --because the one in node just plain sucks
+
+var def = Object.defineProperty || function(object, property, desc){
+	object[property] = desc.value
+	return object
+}
+
+var emi = function emi(obj){
+	if (obj && !(this instanceof emi)){
+		for (var p in emi.prototype) obj[p] = emi.prototype[p]
+		return emi
+	}
+}
+
+emi.prototype.on = function(event, fn){
+	var listeners = this._listeners || def(this, "_listeners", {value: {}})._listeners,
+		events = listeners[event] || (listeners[event] = [])
+	if (!events.length || events.indexOf(fn) === -1) events.push(fn)
+	return this
+}
+
+emi.prototype.off = function(event, fn){
+	var listeners = this._listeners, events
+	if (listeners && (events = listeners[event]) && events.length) for (var i = events.length; i--;) if (events[i] === fn){
+		events.splice(i, 1)
+		break
+	}
+	return this
+}
+
+emi.prototype.emit = function(event){
+	var listeners = this._listeners, events
+	if (listeners && (events = listeners[event])){
+		events = events.slice()
+		var args = Array.prototype.slice.call(arguments, 1)
+		for (var i = 0, l = events.length; i < l; i++) events[i].apply(this, args)
+	}
+	return this
+}
+
+module.exports = emi
